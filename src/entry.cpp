8f8cfdd5421dd8612e1e966ddb7aa6a1cd29e58f
Akasha <akashic.theory@gmail.com>
2024-09-18 13:52:26 -0500

    :page_facing_up: Add License to source file

--- src/entry.cpp
+++ src/entry.cpp
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ *
+ * Apparo, an event-scheduling and reminder Discord bot
+ *
+ * Copyright (c) Akasha Mohan
+ * See full license text at https://github.com/Akashic-Theory/apparo/blob/main/LICENSE
+ * SPDX-License-Identifier: MIT
+ *
+ ******************************************************************************/
+
 #include <iostream>
 
 int main(int argc, char** argv) {
